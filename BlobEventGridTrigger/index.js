e21c87d39b2e7ffd0b4d46e6e0312c09aa69cbf0
Craig Roberts <craig.roberts11@outlook.com>
2023-05-23 06:27:20 +0930

    photos to be added to thumbnails container

--- BlobEventGridTrigger/index.js
+++ BlobEventGridTrigger/index.js
@@ -6,15 +6,15 @@ const ONE_MEGABYTE = 1024 * 1024;
 const uploadOptions = { bufferSize: 4 * ONE_MEGABYTE, maxBuffers: 20 };
 
 module.exports = async function (context, eventGridEvent, inputBlob) {
-  const containerName = context.bindingData.data.url.split("/")[3];
+  const containerName = "thumbnails";
   const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
   const blobUrl = context.bindingData.data.url;
-  const originalBlobName = blobUrl.slice(blobUrl.lastIndexOf("/") + 1);
+  const blobName = blobUrl.slice(blobUrl.lastIndexOf("/") + 1);
 
   // separate the file name from extension and insert "-thumbnail" at the end of the file name before the extension
-  const fileName = originalBlobName.slice(0, originalBlobName.lastIndexOf("."));
-  const fileExtension = originalBlobName.slice(originalBlobName.lastIndexOf("."));
-  const blobName = fileName + "-thumbnail" + fileExtension;
+  // const fileName = originalBlobName.slice(0, originalBlobName.lastIndexOf("."));
+  // const fileExtension = originalBlobName.slice(originalBlobName.lastIndexOf("."));
+  // const blobName = fileName + "-thumbnail" + fileExtension;
 
   const widthInPixels = 250;
   Jimp.read(inputBlob).then((thumbnail) => {
