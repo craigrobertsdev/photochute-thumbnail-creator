6233bc84aa8d8d63120707e521faab94f8e342b7
Craig Roberts <craig.roberts11@outlook.com>
2023-05-23 06:06:43 +0930

    fixed file name

--- BlobEventGridTrigger/index.js
+++ BlobEventGridTrigger/index.js
@@ -5,16 +5,15 @@ const { BlockBlobClient } = require("@azure/storage-blob");
 const ONE_MEGABYTE = 1024 * 1024;
 const uploadOptions = { bufferSize: 4 * ONE_MEGABYTE, maxBuffers: 20 };
 
-const containerName = context.bindingData.data.url.split("/")[3];
-const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
-
 module.exports = async function (context, eventGridEvent, inputBlob) {
+  const containerName = context.bindingData.data.url.split("/")[3];
+  const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
   const blobUrl = context.bindingData.data.url;
   const originalBlobName = blobUrl.slice(blobUrl.lastIndexOf("/") + 1);
 
   // separate the file name from extension and insert "-thumbnail" at the end of the file name before the extension
   const fileName = originalBlobName.slice(0, originalBlobName.lastIndexOf(".") - 1);
-  const fileExtension = originalBlobName.slice(originalBlobName.lastIndexOf("."));
+  const fileExtension = originalBlobName.slice(originalBlobName.lastIndexOf(".") + 1);
   const blobName = fileName + "-thumbnail" + fileExtension;
 
   const widthInPixels = 250;
