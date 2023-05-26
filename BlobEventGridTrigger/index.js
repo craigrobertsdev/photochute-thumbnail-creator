f85642ba577e0f666e4274488c9f0746da152059
Craig Roberts <craig.roberts11@outlook.com>
2023-05-27 05:40:40 +0930

    reverted to thumbnails being storage location

--- BlobEventGridTrigger/index.js
+++ BlobEventGridTrigger/index.js
@@ -6,16 +6,16 @@ const ONE_MEGABYTE = 1024 * 1024;
 const uploadOptions = { bufferSize: 4 * ONE_MEGABYTE, maxBuffers: 20 };
 
 module.exports = async function (context, eventGridEvent, inputBlob) {
-  // const containerName = "thumbnails";
+  const containerName = "thumbnails";
   const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
   const blobUrl = context.bindingData.data.url;
-  const containerName = blobUrl.split("/")[3];
-  // const blobName = blobUrl.slice(blobUrl.lastIndexOf("/") + 1);
+  // const containerName = blobUrl.split("/")[3];
+  const blobName = blobUrl.slice(blobUrl.lastIndexOf("/") + 1);
 
   // separate the file name from extension and insert "-thumbnail" at the end of the file name before the extension
-  const fileName = originalBlobName.slice(0, originalBlobName.lastIndexOf("."));
-  const fileExtension = originalBlobName.slice(originalBlobName.lastIndexOf("."));
-  const blobName = fileName + "-thumbnail" + fileExtension;
+  // const fileName = originalBlobName.slice(0, originalBlobName.lastIndexOf("."));
+  // const fileExtension = originalBlobName.slice(originalBlobName.lastIndexOf("."));
+  // const blobName = fileName + "-thumbnail" + fileExtension;
 
   const widthInPixels = 250;
   Jimp.read(inputBlob).then((thumbnail) => {
