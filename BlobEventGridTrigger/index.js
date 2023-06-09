49aed1624b35693c9f101f67059e1115bb8b9819
Craig Roberts <craig.roberts11@outlook.com>
2023-06-09 21:54:06 +0930

    fixed blobName being undefined

--- BlobEventGridTrigger/index.js
+++ BlobEventGridTrigger/index.js
@@ -1,23 +1,16 @@
 const Jimp = require("jimp");
 const stream = require("stream");
 const { BlockBlobClient } = require("@azure/storage-blob");
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
+
 const ONE_MEGABYTE = 1024 * 1024;
 const uploadOptions = { bufferSize: 4 * ONE_MEGABYTE, maxBuffers: 20 };
 
 module.exports = async function (context, eventGridEvent, inputBlob) {
-  // const containerName = "thumbnails";
   const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
   const blobUrl = context.bindingData.data.url;
-  // const blobUrl =
-  //   "https://photochute.blob.core.windows.net/the-walruses-5a8118b0-fbac-11ed-aaed-4bd91ebe2c21/dark_souls-3163c2e0-fbb4-11ed-ac89-3df18d1f362c.jpg";
-  const containerName = blobUrl.split("/")[3];
-  // const blobName = blobUrl.slice(blobUrl.lastIndexOf("/") + 1);
-
-  // separate the file name from extension and insert "-thumbnail" at the end of the file name before the extension
-  const fileName = originalBlobName.slice(0, originalBlobName.lastIndexOf("."));
-  const fileExtension = originalBlobName.slice(originalBlobName.lastIndexOf("."));
-  const blobName = fileName + "-thumbnail" + fileExtension;
+  const blobName = blobUrl.split("/")[4];
+  const containerName = "thumbnails";
 
   const widthInPixels = 250;
   Jimp.read(inputBlob).then((thumbnail) => {
@@ -28,16 +21,15 @@ module.exports = async function (context, eventGridEvent, inputBlob) {
       readStream.end(buffer);
 
       const blobClient = new BlockBlobClient(connectionString, containerName, blobName);
-
       try {
-        const uploadResponse = await blobClient.uploadStream(
+        await blobClient.uploadStream(
           readStream,
           uploadOptions.bufferSize,
           uploadOptions.maxBuffers,
           { blobHTTPHeaders: { blobContentType: "image/jpeg" } }
         );
 
-        await fetch("https://photochute.herokuapp.com", {
+        const response = await fetch("https://photochute.herokuapp.com", {
           headers: {
             "Content-Type": "application/json",
           },
@@ -47,7 +39,7 @@ module.exports = async function (context, eventGridEvent, inputBlob) {
           }),
         });
       } catch (err) {
-        context.log(err.message);
+        context.log.error(err);
       }
     });
   });
